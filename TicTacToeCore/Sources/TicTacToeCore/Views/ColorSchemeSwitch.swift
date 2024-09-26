9c9b307d6168283a5546015ca96932387d6b9c0f
Koshimizu-Takehito <takehito.koshimizu@gmail.com>
2024-09-26 09:52:05 +0900

    リファクタリング

--- TicTacToeCore/Sources/TicTacToeCore/Views/ColorSchemeSwitch.swift
+++ TicTacToeCore/Sources/TicTacToeCore/Views/ColorSchemeSwitch.swift
@@ -3,7 +3,6 @@ import SwiftUI
 // MARK: - ColorSchemeSwitch
 
 struct ColorSchemeSwitch: View {
-    @Environment(\.colorScheme) var defaultScheme
     @Binding var colorScheme: ColorScheme?
     @State private var ratio = 1.0
     @TextColor private var foreground
@@ -31,7 +30,7 @@ struct ColorSchemeSwitch: View {
     }
 
     var isOn: Bool {
-        (colorScheme ?? defaultScheme) == .light
+        colorScheme != .dark
     }
 
     func toggle() {
