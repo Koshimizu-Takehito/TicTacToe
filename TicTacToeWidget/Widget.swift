504d730968c7271e7d6b56aedbe4572b05fc32ae
Koshimizu-Takehito <takehito.koshimizu@gmail.com>
2024-09-28 10:50:52 +0900

    Widget のテンプレートを追加

---
+++ TicTacToeWidget/Widget.swift
@@ -0,0 +1,45 @@
+import WidgetKit
+import SwiftUI
+
+struct TimelineEntry: WidgetKit.TimelineEntry {
+    let date: Date
+}
+
+struct WidgetEntryView : View {
+    var entry: TimelineEntry
+
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Time:")
+                Text(entry.date, style: .time)
+            }
+        }
+    }
+}
+
+struct Widget: SwiftUI.Widget {
+    let kind: String = "TicTacToeWidget"
+
+    var body: some WidgetConfiguration {
+        StaticConfiguration(kind: kind, provider: TimelineProvider()) { entry in
+            if #available(macOS 14.0, iOS 17.0, *) {
+                WidgetEntryView(entry: entry)
+                    .containerBackground(.fill.tertiary, for: .widget)
+            } else {
+                WidgetEntryView(entry: entry)
+                    .padding()
+                    .background()
+            }
+        }
+        .configurationDisplayName("TicTacToe")
+        .description("You can quickly launch TicTacToe.")
+    }
+}
+
+#Preview(as: .systemSmall) {
+    Widget()
+} timeline: {
+    TimelineEntry(date: .now)
+    TimelineEntry(date: .now)
+}
---
+++ TicTacToeWidget/WidgetBundle.swift
@@ -0,0 +1,9 @@
+import WidgetKit
+import SwiftUI
+
+@main
+struct WidgetBundle: SwiftUI.WidgetBundle {
+    var body: some SwiftUI.Widget {
+        Widget()
+    }
+}
